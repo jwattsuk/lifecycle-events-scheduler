988551d1d9ddd35519de40ec07e5e72610ae72ef
Jonathan Watts <github@insufficientmemory.co.uk>
2025-10-09 16:56:27 +0100

    EnableScheduling annotation

--- sample-service-parent/sample-service/src/main/java/com/jwattsuk/sample/LifecycleEventsService.java
+++ sample-service-parent/sample-service/src/main/java/com/jwattsuk/sample/LifecycleEventsService.java
@@ -6,11 +6,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.data.mongo.MongoDataAutoConfiguration;
 import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
-@SpringBootApplication(exclude = {
-        MongoAutoConfiguration.class,
-        MongoDataAutoConfiguration.class
-})
+@SpringBootApplication
+@EnableScheduling
 public class LifecycleEventsService {
     private static final Logger LOG = LoggerFactory.getLogger(LifecycleEventsService.class);
 
